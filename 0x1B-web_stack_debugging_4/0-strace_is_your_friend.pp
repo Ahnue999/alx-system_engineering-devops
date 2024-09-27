3e6e2786250841efa88b02f6b83426137f419503
Ahnue999 <ahnue200572@gmail.com>
2024-09-27 10:59:04 +0300

    commit

--- 0x1B-web_stack_debugging_4/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# an automated puppet fix for finding why Apache is returning 500 error
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
-}
